57acd8b2827114e0d0946c5efb29914ad4114bf6
yashbhambere99 <102457787+yashbhambere99@users.noreply.github.com>
2022-06-02 20:16:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
-
+import streamlit as st
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
